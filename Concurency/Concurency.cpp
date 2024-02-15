bc5780ef427c0056fcbf647c4b9f1289edc346d3
Adetunji Adeniran <AAdeniran001@dundee.ac.uk>
2024-02-15 11:40:12 +0000

    Concurency Demonstration

---
+++ Concurency/Concurency.cpp
@@ -0,0 +1,46 @@
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+void readFile(string filename)
+{
+    ifstream inputFile(filename);
+
+    if (!inputFile.is_open())
+    {
+
+        cerr << "Error opening output file!"
+            << endl;
+
+        return;
+    }
+    string line;
+
+    while (getline(inputFile, line))
+    {
+
+        cout << line << endl;
+
+    }
+
+}
+int main()
+{
+    thread t1(readFile, "file1.txt");
+    thread t2(readFile, "file2.txt");
+    
+   t1.join();
+   t2.join();
+   
+    //readFile("file1.txt");
+    //readFile("file2.txt");
+    
+    return 0;
+
+
+
+}
